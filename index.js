5551c6eae75551bcee77ef58a5f89ff940b36a44
Soporte3w <167887286+Soporte3w@users.noreply.github.com>
2025-01-08 10:42:55 -0500

    actualizado

--- index.js
+++ index.js
@@ -21,6 +21,55 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
 // Escucha las conexiones entrantes
 wss.on('connection', (ws) => {
     console.log('Cliente conectado');
+
+    // Escucha los mensajes entrantes del cliente
+    ws.on('message', async (message) => {
+        if (message.toString() === 'ejecutar_consulta') {
+            try {
+                // Conectar a la base de datos
+                const pool = await sql.connect(config);
+
+                // Primera consulta: Obtener el primer registro con fecha pendiente
+                const result1 = await pool.request().query(`
+              SELECT TOP 1 *
+FROM Tab_Sendmessage
+WHERE fecha_pendiente IS NOT NULL
+  AND fecha_pendiente > DATEADD(MINUTE, -1, 
+                               DATEADD(MILLISECOND, -DATEPART(MILLISECOND, GETUTCDATE()), 
+                               DATEADD(SECOND, -DATEPART(SECOND, GETUTCDATE()), 
+                               DATEADD(HOUR, -5, GETUTCDATE()))))
+  AND idestado <> 6
+ORDER BY fecha_pendiente ASC;
+            `);
+
+                // Segunda consulta: Obtener registros con idestado = 4 o 0 y empresa = 'Yego'
+                const result2 = await pool.request().query(`
+               SELECT *
+FROM Tab_Sendmessage
+WHERE (idestado = 4 OR idestado = 0)
+  AND empresa = 'Yego'
+  AND idestado <> 6;
+            `);
+
+                // Crear un objeto para combinar los resultados
+                const response = {
+                    fechaPendiente: result1.recordset.length > 0 ? result1.recordset : 'No hay registros con fecha pendiente',
+                    estadoYego: result2.recordset.length > 0 ? result2.recordset : 'No hay registros con idestado 4 o 0 para Yego'
+                };
+
+                // Enviar la respuesta combinada
+                ws.send(JSON.stringify(response));
+
+            } catch (err) {
+                console.error('Error al ejecutar la consulta:', err);
+                ws.send('Error al ejecutar la consulta');
+            }
+        } else {
+            console.log("error")
+        }
+    });
+
+    // Escucha cuando el cliente se desconecta
     ws.on('close', () => {
         console.log('Cliente desconectado');
     });
